b6ef9c17fd4703e18cf1d66c12d3690b2ae30a81
vincent liao <vincentliao@kkbox.com>
2014-04-24 17:21:26 +0800

    answer for SQA members

---
+++ top_n.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# description : Show those largest n numbers from a file. 
+#               seems like a unix command: cat file.txt | sort -n -r | head -n 3
+# author      : vincentliao
+# date        : 20140424
+
+import sys
+
+TOP_N = 3
+top = [None] * (TOP_N+1)
+
+def escalate(top):
+	for i in reversed(range(1, TOP_N+1)):
+		if top[i] > top[i-1]: 
+			tmp = top[i-1]
+			top[i-1] = top[i] 
+			top[i] = tmp
+
+# read number from file(command argument)
+fd = open(sys.argv[1], "r")
+numbers = []
+while True:
+	line = fd.readline().strip()
+	if line == '':
+		break
+				
+	top[TOP_N] = int(line) 
+	escalate(top)
+
+for n in top[:TOP_N]:
+	print n
